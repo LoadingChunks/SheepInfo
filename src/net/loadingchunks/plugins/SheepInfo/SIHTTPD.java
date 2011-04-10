52fdb843a6f7b6063a856e50369eedc41eaf11e0
Ben Woodford <cue@loadingchunks.net>
2011-04-10 14:21:10 +0100

    Added basic web server, returns basic info such as loaded chunks, living entities, as well as a list of players and their information.

---
+++ src/net/loadingchunks/plugins/SheepInfo/SIHTTPD.java
@@ -0,0 +1,87 @@
+package net.loadingchunks.plugins.SheepInfo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+import org.bukkit.World;
+import org.json.simple.*;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import net.loadingchunks.plugins.SheepInfo.SheepInfo;
+
+public class SIHTTPD {
+    private final SheepInfo plugin;
+    private final SIJSON infoget = new SIJSON(this.plugin);
+    private HttpServer server;
+
+    public SIHTTPD (SheepInfo plugin) {
+        this.plugin = plugin;
+    }
+    
+    public boolean Listen()
+    {
+		try {
+			this.server = HttpServer.create(new InetSocketAddress(Integer.parseInt(this.plugin.siConfig.get("bind_port"))), 0);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+    	this.server.createContext("/info", new SInfoHandler(this));
+    	this.server.createContext("/", new SIDefaultHandler());
+    	this.server.setExecutor(null);
+    	this.server.start();
+    	return true;
+    }
+    
+    public void Kill()
+    {
+    	this.server.stop(0);
+    }
+    
+    static class SInfoHandler implements HttpHandler {
+    	private final SIHTTPD httpd;
+    	
+    	public SInfoHandler(SIHTTPD instance)
+    	{
+    		this.httpd = instance;
+    	}
+    	
+    	public void handle(HttpExchange t) throws IOException {
+    		JSONObject response_object = new JSONObject();
+    		JSONObject worlds_object = new JSONObject();
+    		
+    		for( World w : this.httpd.plugin.worlds)
+    		{
+    			worlds_object.clear();
+    			worlds_object.put("core", this.httpd.infoget.Info(w));
+    			worlds_object.put("players", this.httpd.infoget.Players(w));
+    			response_object.put(w.getName(), worlds_object);
+    		}
+    		
+    		String response = response_object.toJSONString();
+
+    		t.sendResponseHeaders(200, response.length());
+    		OutputStream os = t.getResponseBody();
+    		os.write(response.getBytes());
+    		os.close();
+    	}
+    }
+    
+    static class SIDefaultHandler implements HttpHandler {
+    	public void handle(HttpExchange t) throws IOException {
+    		String response = "Try /info to get MC Server Info.";
+    		t.sendResponseHeaders(200, response.length());
+    		OutputStream os = t.getResponseBody();
+    		os.write(response.getBytes());
+    		os.close();
+    	}
+    }
+}
---
+++ src/net/loadingchunks/plugins/SheepInfo/SheepInfo.java
@@ -0,0 +1,65 @@
+
+package net.loadingchunks.plugins.SheepInfo;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.util.config.Configuration;
+
+/**
+ * GuardWolf Ban System plugin for Bukkit
+ *
+ * @author Cue
+ */
+public class SheepInfo extends JavaPlugin {
+	public final HashMap<String, String> siConfig = new HashMap<String, String>();
+	public final SIHTTPD httpd = new SIHTTPD(this);
+	public List<World> worlds;
+
+    public void onDisable() {
+        System.out.println("[SHEEPINFO] Stopping...");
+        this.httpd.Kill();
+        System.out.println("[SHEEPINFO] Killed HTTPD Server.");
+    }
+
+    public void onEnable() {        
+        // Register events
+        
+        PluginManager pm = getServer().getPluginManager();
+        
+        // Get the config.
+        
+        System.out.println("[SHEEPINFO] Loading config file plugins/SheepInfo/config.yml...");
+        Configuration _config = new Configuration(new File("plugins/SheepInfo/config.yml"));
+        
+        _config.load();
+        
+        System.out.println("Loaded GuardWolf Config Successfully!");
+        
+        siConfig.put("bind_addr", _config.getString("sheep.bind.address"));
+        siConfig.put("bind_port", _config.getString("sheep.bind.port"));
+        
+        System.out.println("SheepInfo Config saved to memory.");
+        
+        worlds = this.getServer().getWorlds();
+        System.out.println("[SHEEPINFO] Generated Worlds List.");
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+        
+        System.out.println("[SHEEPINFO] Starting SheepInfo HTTPD on " + siConfig.get("bind_addr") + ":" + siConfig.get("bind_port"));
+        if(!httpd.Listen())
+        	System.out.println("[SHEEPINFO] Error starting SheepInfo HTTPD!");
+        else
+        	System.out.println("[SHEEPINFO] SheepInfo HTTPD successfully listening!");
+    }
+}
